fc9a6baf27a4b1976e36e697073656b2a153d2ba
Aaron <chairman@defend-network.org>
2026-08-14 21:47:18 -0400

    refactor: generalize shared platform application registry

--- shared_platform/__init__.py
+++ shared_platform/__init__.py
@@ -1,6 +1,11 @@
 """Application-neutral boundaries shared by DEFEND and SCS."""
 
-from .application import ApplicationContext, ApplicationId, validate_application_pair
+from .application import (
+    ApplicationContext,
+    ApplicationId,
+    validate_application_pair,
+    validate_applications,
+)
 from .secrets import NamespacedSecrets
 from .services import DeploymentProfile, RouteProfile, ServiceProfile, validate_deployment
 from .phase0 import build_phase0_deployment, build_scs_process_specs, phase0_contexts
@@ -17,4 +22,5 @@ __all__ = [
     "build_scs_process_specs",
     "phase0_contexts",
     "validate_application_pair",
+    "validate_applications",
 ]
